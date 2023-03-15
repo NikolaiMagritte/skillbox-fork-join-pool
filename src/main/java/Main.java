4da2e299a15af272dd9d3b89ce2edcc00b5735ab
Nikolai Magritte <Nmagritte@ya.ru>
2023-03-15 17:48:12 +0300

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "https://skillbox.ru/";
+        String pathToSiteMapFile = "data/siteMap.txt";
+        SiteMap siteMap = new SiteMap(url);
+        SiteMapRecursiveAction task = new SiteMapRecursiveAction(siteMap);
+        new ForkJoinPool().invoke(task);
+
+        try {
+            FileOutputStream stream = new FileOutputStream(pathToSiteMapFile);
+            String siteMapFile = createSiteMapString(siteMap, 0);
+            stream.write(siteMapFile.getBytes());
+            stream.flush();
+            stream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String createSiteMapString(SiteMap siteMap, int indent) {
+        String tab = String.join("", Collections.nCopies(indent, "\t"));
+        StringBuilder result = new StringBuilder(tab + siteMap.getUrl());
+        siteMap.getSiteMapChildrens().forEach(child -> result.append("\n")
+                .append(createSiteMapString(child, indent + 1)));
+        return result.toString();
+    }
+}
